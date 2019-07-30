324043d3d24a9a471ce5fc7e329e3052f1f3e1dd
Ilya Mironov <koiiiara@me.com>
2019-07-30 14:50:57 +0300

    Initial Commit

---
+++ Place/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Place
+//
+//  Created by Илья Миронов on 30/07/2019.
+//  Copyright © 2019 Ilya Mironov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
